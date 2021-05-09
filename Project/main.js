f07841b8f9ee3d264eda0c469afdf16d6323f4a1
Madelineh <mggoepp@gmail.com>
2021-05-09 13:17:39 -0700

    Add files via upload

--- Project/main.js
+++ Project/main.js
@@ -1,19 +1,30 @@
 var express = require('express');
-var mysql = require('./dbcon.js');
-var bodyParser = require('body-parser');
 
 var app = express();
-var handlebars = require('express-handlebars').create({
-        defaultLayout:'main',
-        });
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+// const puppeteer = require('puppeteer');
 
 app.engine('handlebars', handlebars.engine);
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
-app.set('port', 26450); //26450 process.argv[2]
-app.set('mysql', mysql);
-app.use('/', express.static('public'));
+app.set('port', 26450);
+
+
+
+
+//Routes
+
+app.use('/', require('./views/scraper.js'));
+
+// app.get('/',function(req,res){
+//   res.render('home.handlebars'); 
+//   // require('./scraper.js');
+//   // require('./scraper.js')
+//   // res.render('index.html') 
+// });
+
+// app.get('/search-figure',function(req,res){
+//   res.render('search-figure');
+// });
 
 app.use(function(req,res){
   res.status(404);
@@ -22,6 +33,7 @@ app.use(function(req,res){
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
+  res.type('plain/text');
   res.status(500);
   res.render('500');
 });
@@ -30,46 +42,6 @@ app.listen(app.get('port'), function(){
   console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
 
+const puppeteer = require('puppeteer');
 
-
-// /*
-//     THIS CODE IS HEAVILY BORROWED FROM https://github.com/wolfordj/CS290-Server-Side-Examples
-//     Code implemented is also borrowed from w3schools.com and stackOverflow.
-// */
-
-// var express = require('express');
-// var mysql = require('./dbcon.js');
-// var bodyParser = require('body-parser');
-
-// var app = express();
-// var handlebars = require('express-handlebars').create({
-//         defaultLayout:'main',
-//         });
-
-
-// app.engine('handlebars', handlebars.engine);
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use('/static', express.static('public'));
-// app.set('view engine', 'handlebars');
-// app.set('port', 26555); //26450 process.argv[2]
-// app.set('mysql', mysql);
-// app.use('/customers', require('./customers.js'));//customers.js
-// app.use('/search', require('./scraper.js'));
-// app.use('/', express.static('public'));
-
-
-// app.use(function(req,res){
-//   res.status(404);
-//   res.render('404');
-// });
-
-// app.use(function(err, req, res, next){
-//   console.error(err.stack);
-//   res.status(500);
-//   res.render('500');
-// });
-
-// app.listen(app.get('port'), function(){
-//   console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
-// });
-
+ 
--- Project/scraper.js
+++ Project/scraper.js
@@ -1,11 +1,47 @@
-const rp = require('request-promise');
-const url = 'https://en.wikipedia.org/wiki/Marilyn';
+const puppeteer = require('puppeteer');
 
-rp(url)
-  .then(function(html){
-    //success!
-    console.log(html);
-  })
-  .catch(function(err){
-    //handle error
-  });
+        function test(first, last){
+    
+            document.getElementById("functest").innerHTML="It works, " + first.value + last.value;
+        }
+
+        // async function scrapeProduct(first, last){
+        //     // const puppeteer = require('puppeteer');
+        //      document.getElementById("functest").innerHTML="#1 It works, " + first.value + last.value;
+
+
+        //     const browser = await puppeteer.launch();
+        //     document.getElementById("functest").innerHTML="#2a It works, " + first.value + last.value;
+        //     const page = await browser.newPage();
+        //     document.getElementById("functest").innerHTML="#2b It works, " + first.value + last.value;
+        //     const url = "https://en.wikipedia.org/wiki/" + first + "_" + last;
+        //     document.getElementById("functest").innerHTML="#2c It works, " + first.value + last.value;
+        //     await page.goto(url);
+        //     document.getElementById("functest").innerHTML="#2d It works, " + first.value + last.value;
+
+        //     const [el] = await page.$x('//*[@id="mw-content-text"]/div[1]/table/tbody/tr[2]/td/a/img');
+        //     const src = await el.getProperty('src');
+        //     const srcTxt = await src.jsonValue();
+        //     document.getElementById("functest").innerHTML="#3 It works, " + first.value + last.value;
+
+        //     const [el2] = await page.$x('//*[@id="mw-content-text"]/div[1]/p[3]');
+        //     const txt = await el2.getProperty('textContent');
+        //     const rawTxt = await txt.jsonValue();
+        //     document.getElementById("functest").innerHTML="#4 It works, " + first.value + last.value;
+
+        //     const [el3] = await page.$x('//*[@id="toc"]/ul');
+        //     const txt2 = await el3.getProperty('textContent');
+        //     const contents = await txt2.jsonValue();
+
+        //     var test="This works!";
+
+        //     // console.log({srcTxt, rawTxt, contents});
+        //     document.getElementById("mm_image").innerHTML=srcTxt;
+        //     document.getElementById("mm_bio").innerHTML=rawTxt;
+        //     document.getElementById("mm_contents").innerHTML=contents;
+
+        //    document.getElementById("functest").innerHTML="#5 It works, " + first.value + last.value;
+
+
+        //     browser.close();
+        // }
