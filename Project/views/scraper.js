a4294558e6c1821f18bfaba540377ef10e3645b0
Madelineh <mggoepp@gmail.com>
2021-05-21 21:22:31 -0700

    Adding the most recent file with new functions

--- Project/views/scraper.js
+++ Project/views/scraper.js
@@ -1,9 +1,29 @@
 const puppeteer = require('puppeteer');
 
-        // function test(first, last){
-    
-        //     document.getElementById("functest").innerHTML="It works, " + first.value + last.value;
-        // }
+        
+function test2(first, last){
+
+    document.getElementById("person1").innerHTML="Shot Marilyns";
+    document.getElementById("person2").innerHTML="Marilyn Diptych";
+    document.getElementById("person3").innerHTML="Marilyn Manson";
+    document.getElementById("person4").innerHTML="Marilyn Manson (band)";
+    document.getElementById("person5").innerHTML="Mary Lynne (song)";
+    // document.getElementById("mm_people6").innerHTML="r";
+    // document.getElementById("mm_people7").innerHTML="e";
+    // document.getElementById("mm_people8").innerHTML="e";
+    // document.getElementById("mm_people9").innerHTML="r";
+    // document.getElementById("mm_people10").innerHTML="e";
+    document.getElementById("mm").innerHTML="Marilyn Monroe";
+
+};
+
+
+function test1(){
+
+    document.getElementById("mm_bio").innerHTML="Marilyn Monroe (/ˈmærɪlɪn mənˈroʊ/; born Norma Jeane Mortenson; June 1, 1926 – August 4, 1962) was an American actress, model, and singer. Famous for playing comedic 'blonde bombshell' characters, she became one of the most popular sex symbols of the 1950s and early 1960s and was emblematic of the era's sexual revolution. She was a top-billed actress for only a decade, but her films grossed $200 million (equivalent to $2 billion in 2019) by the time of her death in 1962.[1] Long after her death, she continues to be a major icon of pop culture.[2] In 1999, the American Film Institute ranked Monroe sixth on its list of the greatest female screen legends from the Golden Age of Hollywood.";
+    document.getElementById("mm_people").innerHTML="t";
+
+};
 
 
 
@@ -11,11 +31,16 @@ async function scrapeProduct(first, last){
 
     document.getElementById("functest").innerHTML="#1a It works, " + first.value + last.value;
 
-    const url = "https://en.wikipedia.org/wiki/" + first + "_" + last;
+    // const url = "https://en.wikipedia.org/wiki/Marilyn_Monroe";
+
+    const url = "https://en.wikipedia.org/wiki/" + first.value + "_" + last.value;
+
+    document.getElementById("functest").innerHTML="#1b It works, " + first.value + last.value;
 
 
     (async () => {
-         document.getElementById("functest").innerHTML="#1b It works, " + first.value + last.value;
+
+        document.getElementById("functest").innerHTML="#1c It works, " + first.value + last.value;
 
 
 
@@ -51,6 +76,6 @@ async function scrapeProduct(first, last){
 
        document.getElementById("functest").innerHTML="#5 It works, " + first.value + last.value;
 
-   });
+   })();
     // browser.close();
 };
